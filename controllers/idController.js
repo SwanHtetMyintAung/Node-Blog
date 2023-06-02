3c4e924e0be88307a23558edcb0c135e225a38c7
SwanHtetMyintAung <swan60953@gmail.com>
2023-06-02 17:06:12 +0630

    minor bug fixes

--- controllers/idController.js
+++ controllers/idController.js
@@ -2,28 +2,27 @@ const INFO = require('../models/Info');
 const USER = require('../models/User')
 const mongoose = require('mongoose')
 
-function getOnlyOneBlog(req,res){
+async function getOnlyOneBlog(req,res){
     let searchId = req.params.id;
 
     if(!mongoose.Types.ObjectId.isValid(searchId)){
         res.status(404).render('404' , {title : "404"});
         return;
     }
-    INFO.findById(searchId)
-    .then(result =>{
-        res.render('blog/blogInfo' , {title:"blog" , info : result })
-    })
-    .catch(err => console.log(err))
+    let singleBlog = await INFO.findById(searchId);
+    let singleUser = await USER.find({name:singleBlog.author})
+    res.render('blog/blogInfo' , {title:singleBlog.title , info : singleBlog , blogAuthor : singleUser[0] })
 }
 
 async function getOnlyOneUser(req,res){
   let searchId = req.params.id;
-
+  console.log(searchId ,"One User****************************")
   if(!mongoose.Types.ObjectId.isValid(searchId)){
       res.status(404).render('404' , {title : "404"});
       return;
   }
   let oneUser = await USER.findById(searchId)
+  
   let infos = await INFO.find({author : oneUser.name})
   res.render('user/userInfo' , {
     title : oneUser.name , 
